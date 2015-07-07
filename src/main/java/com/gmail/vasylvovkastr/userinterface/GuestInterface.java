1603a420a9cedce165bd5b01f501d4338e550e50
vasyl <vasylvovkastr@gmail.com>
2015-07-07 23:06:47 +0300

    first UI commit

--- src/main/java/com/gmail/vasylvovkastr/userinterface/GuestInterface.java
+++ src/main/java/com/gmail/vasylvovkastr/userinterface/GuestInterface.java
@@ -1,6 +1,26 @@
 package com.gmail.vasylvovkastr.userinterface;
 
-public class GuestInterface {
+import com.gmail.vasylvovkastr.supportobjects.Kategory;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GuestInterface extends JFrame {
+
+    public GuestInterface(){
+        this.setSize(1000, 500);
+        this.setTitle("Guest Mode");
+
+        DefaultListModel listModel = new DefaultListModel();
+        listModel.addElement("Alcohol");
+        listModel.addElement("Grossery");
+
+        JList list = new JList(listModel);
+        list.setBounds(10,10,440,490);
+        this.add(list);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public boolean loginUser() {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -10,5 +30,11 @@ public class GuestInterface {
     }
 
     public void searchProducts() {
+    }
+
+
+    public static void main(String... args) {
+        JFrame guestForm = new GuestInterface();
+        guestForm.setVisible(true);
     }
 }
