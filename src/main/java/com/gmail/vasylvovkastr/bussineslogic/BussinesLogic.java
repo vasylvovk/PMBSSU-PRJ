a9036acc5ef337044b698c5ee14e9b02c78f0d3a
vasyl <vasylvovkastr@gmail.com>
2015-07-23 02:14:33 +0300

    add hibernate

--- src/main/java/com/gmail/vasylvovkastr/bussineslogic/BussinesLogic.java
+++ src/main/java/com/gmail/vasylvovkastr/bussineslogic/BussinesLogic.java
@@ -1,5 +1,11 @@
 package com.gmail.vasylvovkastr.bussineslogic;
 
+import com.gmail.vasylvovkastr.dataaccessobject.HibernateUtil;
+import com.gmail.vasylvovkastr.supportobjects.User;
+import java.awt.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 public class BussinesLogic {
 
     public void checkOrder() {
@@ -16,4 +22,67 @@ public class BussinesLogic {
 
     public void checkUser() {
     }
+
+    private static List userList() {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        List users = (List) session.createQuery("from User").list();
+        session.close();
+        return users;
+    }
+
+    private static User readUser(Long id) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        User user = (User) session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    private static User saveUser(User user) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        int id = (int) session.save(user);
+        user.setIdUser(id);
+
+        session.getTransaction().commit();
+
+        session.close();
+
+        return user;
+    }
+
+    private static User updateUser(User user) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.merge(user);
+
+        session.getTransaction().commit();
+
+        session.close();
+        return user;
+
+    }
+
+    private static void deleteUser(User user) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+
+        session.beginTransaction();
+
+        session.delete(user);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
 }
